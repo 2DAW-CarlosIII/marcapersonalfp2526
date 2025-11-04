1ebca1d40075b1ed3bd1f9268681130cc810916f
Adrian_C <3341802@alu.murciaeduca.es>
2025-11-04 09:55:46 +0100

    ejrcicioRutas resuelto

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
 });
+
+Route::get('login', function () {
+    return "Login usuario";
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', function () {
+        return "Listado proyectos";
+    });
+
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto " . $id;
+    })-> where('id', '[0-9]+');
+
+    Route::get('create', function () {
+        return "Añadir proyecto";
+    });
+
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto " . $id;
+    })-> where('id', '[0-9]+');
+});
+
+Route::get('perfil/{id?}', function ($id = null) {
+    if ($id) {
+        return "Visualizar el currículo de " . $id;
+    } else {
+        return "Visualizar el currículo propio";
+    }
+
+})-> where('id', '[0-9]+');
