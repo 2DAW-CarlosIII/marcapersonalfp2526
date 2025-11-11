98cca1a26034e88627fa7df3f5a722845a8cbcca
Hugo Martínez Ramos <1692000@alu.murciaeduca.es>
2025-11-11 08:52:24 +0100

    dopetrope adaptado a laravel

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+@extends('dopetrope.master')
+
+
+<html>
+    <head>
+        <title>Mi Web</title>
+    </head>
+    <body>
+        @section('menu')
+            Contenido del menu
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
