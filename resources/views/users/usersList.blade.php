324972aa26c3ce711e6ca9cfeb93354874d8727e
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2025-11-11 09:43:33 +0100

    varias vistas con dopetrope

--- resources/views/users/usersList.blade.php
+++ resources/views/users/usersList.blade.php
@@ -1,3 +1,10 @@
-@foreach ($users as $user)
+@extends('dopetrope.master')
+
+
+@section('content')
+    @foreach ($users as $user)
     <li>Usuario {{ $user['name'] }} con identificador: {{ $user['id'] }}</li>
 @endforeach
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/users', function () {
+    return view('users.usersList',[
+        'users'=> [
+            ['id'=> 1, 'name'=>'Ana'],
+            ['id'=> 2, 'name'=>'Fatima'],
+        ]
+    ]);
+});
 
 // ----------------------------------------
 Route::get('login', function () {
