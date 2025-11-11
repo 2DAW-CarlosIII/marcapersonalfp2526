2a3a5d288b4c2c4aa964de2cd0dba611119d1651
Adrian_C <3341802@alu.murciaeduca.es>
2025-11-11 09:44:05 +0100

    Varias vistas con dopetrope

--- resources/views/users/usersList.blade.php
+++ resources/views/users/usersList.blade.php
@@ -1,5 +1,16 @@
-<ul>
-    @foreach ($users as $user)
-        <li>Usuario {{ $user['name'] }} con identificador: {{ $user ['id'] }}</li>
-    @endforeach
-</ul>
+@extends('dopetrope.master')
+
+@section('menu')
+    @parent
+    <li>Opción adicional</li>
+@endsection
+
+@section('content')
+    <ul>
+        @foreach ($users as $user)
+            <li>Usuario {{ $user['name'] }} con identificador: {{ $user ['id'] }}</li>
+        @endforeach
+    </ul>
+@endsection
+
+
