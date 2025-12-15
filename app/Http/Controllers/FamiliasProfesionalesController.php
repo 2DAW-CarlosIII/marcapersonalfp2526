e2b65d42f45f8e86891a51068950aa2216684416
Adrian Cantero <1832726@alu.murciaeduca.es>
2025-12-15 09:53:39 +0100

    Gestión de ficheros de familias profesionales

--- app/Http/Controllers/FamiliasProfesionalesController.php
+++ app/Http/Controllers/FamiliasProfesionalesController.php
@@ -36,7 +36,7 @@ class FamiliasProfesionalesController extends Controller
             ->with('familiaProfesional',  $familias_profesionales);
     }
 
-    public function postCreate(Request $request)
+    public function store(Request $request)
     {
         $familiaProfesional  = new FamiliaProfesional();
         $familiaProfesional ->codigo = $request->input('codigo');
@@ -45,11 +45,16 @@ class FamiliasProfesionalesController extends Controller
 
         return redirect()->route('familias.show', ['id' => $familiaProfesional->id]);
     }
-    public function putCreate(Request $request, $id)
+    public function update(Request $request, $id)
     {
         $familiaProfesional  = FamiliaProfesional::findOrFail($id);
         $familiaProfesional ->codigo = $request->input('codigo');
         $familiaProfesional ->nombre = $request->input('nombre');
+
+        if ($request->hasFile('imagen')){
+            $path = $request->file('imagen')->store('imagenes', ['disk' => 'public']);
+            $familiaProfesional->imagen = $path;
+        }
         $familiaProfesional ->save();
 
         return redirect()->route('familias.show', ['id' => $familiaProfesional->id]);
--- resources/views/familiasProfesionales/show.blade.php
+++ resources/views/familiasProfesionales/show.blade.php
@@ -5,7 +5,13 @@
 
         <div class="col-sm-4">
 
-            <img src="/images/mp-logo.png" style="height:100px" />
+            @if ($familiaProfesional->imagen)
+                <img width="300" style="height:300px" src="{{ Storage::url($familiaProfesional->imagen) }}" alt="imagen"
+                    class="img-thumbnail">
+            @else
+                <img width="300" style="height:300px" alt="Curriculum-vitae-warning-icon"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Curriculum-vitae-warning-icon.svg/256px-Curriculum-vitae-warning-icon.svg.png">
+            @endif
 
         </div>
         <div class="col-sm-8">
@@ -13,7 +19,7 @@
             <h2><b>Nombre: </b>{{ $familiaProfesional->nombre }}</h2>
             <h4><b>Codigo: </b>{{ $familiaProfesional->codigo }}</h4>
 
-            <a href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getEdit'], ['id'=>$familiaProfesional->id]) }}"
+            <a href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getEdit'], ['id' => $familiaProfesional->id]) }}"
                 class="button primary"> Editar
 
             </a>
