77093d1945d9d42221ce2df6fa171d5c67e3939e
Youssef <8957457@alu.murciaeduca.es>
2025-12-15 09:11:54 +0100

    prepatando las familias profesionales

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call(FamiliasProfesionalesTableSeeder::class);
 
         self::seedProyectos();
         $this->command->info('Tabla catálogo inicializada con datos!');
