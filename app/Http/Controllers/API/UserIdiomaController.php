4bc91d18bb903263f9a1581927542fbcc23d8b1f
Gabriel <2866037@alu.murciaeduca.es>
2026-01-28 09:47:56 +0100

    ejercicio de relaciones m-m users-idiomas pasando test

---
+++ app/Http/Controllers/API/UserIdiomaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\IdiomaResource;
+use App\Http\Resources\UserIdiomaResource;
+use App\Models\Idioma;
+use Illuminate\Http\Request;
+
+class UserIdiomaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return UserIdiomaResource::collection(
+            UserIdioma::orderBy($request->sort ?? 'id', $request->order ?? 'asc')
+            ->paginate($request->per_page));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userIdioma = json_decode($request->getContent(), true);
+
+        $userIdioma = Idioma::create($userIdioma);
+
+        return new IdiomaResource($userIdioma);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserIdioma $userIdioma)
+    {
+        return new IdiomaResource($userIdioma);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserIdioma $userIdioma)
+    {
+        $userIdiomaData = json_decode($request->getContent(), true);
+        $userIdioma->update($userIdiomaData);
+
+        return new IdiomaResource($userIdioma);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserIdioma $userIdioma)
+    {
+        try {
+            $userIdioma->delete();
+            return response()->json(null, 204);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error: ' . $e->getMessage()
+            ], 400);
+        }
+    }
+}
---
+++ database/migrations/2026_01_27_000003_create_idioma_users_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+     {
+         Schema::create('idiomas_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idioma_id');
+            $table->unsignedBigInteger('user_id');
+            // FK para idiomas
+            $table->foreign('idioma_id')->references('id')->on('idiomas')->onDelete('cascade');
+            // FK para users
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+             $table->timestamps();
+         });
+     }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('idiomas_users');
+    }
+};
