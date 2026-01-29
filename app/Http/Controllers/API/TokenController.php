88c065e2980520c9b8a94de498d02bdc935deec5
Paul <2907767@alu.murciaeduca.es>
2026-01-29 10:47:11 +0100

    Autenticación con Tokens

---
+++ app/Http/Controllers/API/TokenController.php
@@ -0,0 +1,53 @@
+<?php
+// in app/Http/Controllers/API/TokenController.php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class TokenController extends Controller
+{
+    /**
+     * Store a newly created personal access token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // COMPROBAMOS SI EXISTE UN USUARIO CON EL EMAIL OBTENIDO DE REQUEST.
+        $user = User::where('email', $request->email)->first();
+
+        // SI NO EXISTE EL USUARIO, O LA CONTRASEÑA NO ES CORRECTA, DEVOLVEMOS UN ERROR.
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        // SI EL USUARIO EXISTE Y LA CONTRASEÑA ES CORRECTA, GENERAMOS UN TOKEN
+        return response()->json([
+            'token_type' => 'Bearer',
+            'access_token' => $user->createToken('token_name')->plainTextToken // token name you can choose for your self or leave blank if you like to
+        ]);
+    }
+
+    /**
+     * Delete the user's personal access token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
+}
